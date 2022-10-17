ddd2a78fd0650d268035d4eb045ff88a6f072748
Pizzle power electrical <dollipissul1@gmail.com>
2022-10-17 07:07:31 -0700

    third commit

--- main.h
+++ main.h
@@ -25,7 +25,8 @@ int print_character(va_list args);
 int print_string(va_list args);
 int print_percent(__attribute__((unused))va_list args);
 int print_integer(va_list args);
-
+int print_unsignd(unsigned int n);
+int print_unsigned_int(va_list args);
 
 
 /*main functions*/
