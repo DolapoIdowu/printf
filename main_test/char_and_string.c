a187ef8d2bf48e8df13100a1bb067c84825791d2
Pizzle power electrical <dollipissul1@gmail.com>
2022-10-16 17:07:58 -0700

    second commit

---
+++ main_test/char_and_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char a;
+	char *b;
+
+	a = 'H';
+	b = "hello";
+	_printf("I love Jesus\n");
+	return (0);
+}
--- needed_funtions.c
+++ needed_funtions.c
@@ -4,9 +4,55 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ *	if there is error -1.
  */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
+}
+
+/**
+ * print_character - Prints character
+ * @args: list of unknown argument
+ * Return: The amount of characters printed.
+ */
+int print_character(va_list args)
+{
+	_putchar(va_arg(args, int));
+	return (1);
+}
+
+/**
+ * print_string - Prints a string
+ * @args: list of unknown arguments
+ * Return: The amount of characters of the string printed
+ */
+
+int print_string(va_list args)
+{
+	int count;
+	char *str_ptr;
+
+	str_ptr = va_arg(args, char *);
+
+	if (str_ptr == NULL)
+		str_ptr = "(nill)";
+	count = 0;
+	while (str_ptr[count] != '\0')
+	{
+		_putchar(str_ptr[count]);
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * print_percent - Prints a percent symbol
+ * @args: list of unknown arguments.
+ * Return: The amount of characters printed.
+ */
+int print_percent(__attribute__((unused))va_list args)
+{
+	_putchar('%');
+	return (1);
 }
